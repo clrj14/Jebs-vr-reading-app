eebb82ced6b500ff4f1dc62e4e69383273ef3eba
Cody Rowland <33142639+clrj14@users.noreply.github.com>
2020-03-05 03:03:34 -0800

    Initial Commit

---
+++ Project Template/Assets/Scripts/read_me.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class read_me : MonoBehaviour
+{
+    [TextArea(20, 20)]
+    public string Notes = "";
+}
