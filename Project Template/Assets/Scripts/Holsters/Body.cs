24adf4c7c1f211c50212fdc6ed4f3d6cba3e5ba8
jorgejgnz <jorge.jgnz94@gmail.com>
2020-03-28 17:11:00 +0100

    Map and holsters mechanics

---
+++ Project Template/Assets/Scripts/Holsters/Body.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+    public Transform headset;
+    public Transform pointBetweenControllers;
+    public Transform receiver;
+
+    Vector3 rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        receiver.position = headset.position;
+
+        Vector3 receiverAngles = receiver.eulerAngles;
+        Quaternion meanRotation = Quaternion.Lerp(headset.rotation, pointBetweenControllers.rotation, 0.5f);
+        receiverAngles.y = meanRotation.eulerAngles.y;
+        receiver.eulerAngles = receiverAngles;
+    }
+}
